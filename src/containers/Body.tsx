13af17068994cd6422be6ed8b713bdd32836409f
Jhon Erik <javilaortiz@gmail.com>
2023-07-01 17:08:01 -0500

    feat: adding chat input over floating container (#5)

--- src/containers/Body.tsx
+++ src/containers/Body.tsx
@@ -1,6 +1,5 @@
 import React, { useRef, useState } from 'react';
-import CodeComponent from '../components/CodeComponent';
-import CodeRenderer from '../components/CodeRenderer';
+import { CodeComponent, CodeRenderer, PromptInput } from '../components';
 
 import asist from '../assets/asist.gif';
 
@@ -20,7 +19,9 @@ const Body: React.FC = () => {
       <div className="flex h-full">
         <div className={`${show ? 'opacity-0' : 'opacity-100'} absolute transition-opacity duration-500 bottom-0 right-0 w-screen h-40 bg-gray-600 bg-opacity-80 border border-gray-700 rounded-md p-4`}>
           {/* Content for the floating div */}
-          <div className="flex flex-col text-white font-semibold">CHATBOT</div>
+          <div className="flex flex-col text-white font-semibold">
+            <PromptInput />
+          </div>
         </div>
         <div className="w-1/2 bg-gray-600 border border-gray-700 rounded-md p-4 mr-2">
           {/* Content for the first section */}
@@ -35,13 +36,13 @@ const Body: React.FC = () => {
           </div>
         </div>
       </div>     
-        <button 
-          ref={buttonRef} 
-          onClick={handleClick} 
-          className={`absolute bg-black right-0 bottom-0 w-20 h-20 rounded-full m-3`}
-        >
-            <img src={asist} alt="loader" className='rounded-full' />
-        </button>
+      <button 
+        ref={buttonRef} 
+        onClick={handleClick} 
+        className={`absolute bg-black right-0 bottom-0 w-20 h-20 rounded-full m-3 bg-center bg-cover shadow scale-100 transition duration-1300 ease-in-out hover:scale-125`}
+        style={{ backgroundImage: `url(${asist})` }}
+      >
+      </button>
     </div>
   );
 };
