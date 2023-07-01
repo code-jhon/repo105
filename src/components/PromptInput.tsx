4b96969da2a6812000fd63258ee331f14e95548e
jhon avila <javilaortiz@gmail.com>
2023-07-01 17:07:13 -0500

    feat: adding chat input over floating container

---
+++ src/components/PromptInput.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+const PromptInput: React.FC = () => {
+  const [charCount, setCharCount] = useState(0);
+  const [prompt, setPrompt] = useState('');
+  const [inputError, setInputError] = useState(false);
+  const limit = 500;
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if(e.target.value.length <= limit){
+      setCharCount(e.target.value.length);
+      setPrompt(e.target.value);
+      setInputError(false);
+    }else{
+      setInputError(true);
+    }
+  }
+
+  const handleClear = () => {
+    setPrompt('');
+    setCharCount(0);
+    setInputError(false);
+  }
+
+  return (
+    <div>
+      <textarea 
+        className={`bg-gray-800 text-white p-4 rounded h-full w-full pr-10 ${inputError ? 'text-red-400' : ''}`} 
+        name="" id="" 
+        rows={3}
+        placeholder='Type your prompt here...'
+        onChange={handleChange}
+        value={prompt}
+      ></textarea>
+      <span className={`${inputError ? 'text-red-400' : 'text-gray-400'}`}>{`${limit - charCount}`}</span>
+      <button onClick={handleClear} className="pl-5">Clear</button>
+    </div>
+  )
+}
+
+export default PromptInput;
---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+import CodeComponent from "./CodeComponent";
+import CodeRenderer from "./CodeRenderer";
+import PromptInput from "./PromptInput";
+
+export { 
+  CodeComponent, 
+  CodeRenderer,
+  PromptInput
+};
