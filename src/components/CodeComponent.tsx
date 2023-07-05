ec561faf6b8954b7d67847c4cc3dff04e1a031b4
Jhon Erik <javilaortiz@gmail.com>
2023-07-05 12:34:16 -0500

    feat: rendering messages (#7)

--- src/components/CodeComponent.tsx
+++ src/components/CodeComponent.tsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GPTContext } from '../utils/providers/GPTContext';
 
 const CodeComponent: React.FC = () => {
+  const { messages } = useContext(GPTContext);
+  const filteredMessages = messages.filter( (message) => message.role !== 'system');
+  const parseMessage = (message: string) => {
+    // filter the message looking for code inside triplebackticks
+    const tripleQuoteRegex = /```([\s\S]*?)```/;
+    const match = message.match(tripleQuoteRegex);
+    const content = match ? match[1] : "";
+
+    return content;
+
+    // return the code inside the triplebackticks
+  };
+  const handleRender = () => {
+    // render the code inside the triplebackticks
+    alert("HI")
+  };
+  const messagesContent = filteredMessages.map( (message, index) => {
+    const parsedMessage = parseMessage(message.content);
+    let content = ""
+    if(parsedMessage !== "") {
+      content = message.content.replace(parsedMessage, `<pre style="background-color: #AFAFAF; padding: 5px; border-radius: 10px;"><code>${parsedMessage}</code></pre>`);
+      content = content.replace(/```/g, "<br/>");
+    }else{
+      content = message.content;
+    }
+    return (
+      <div key={index} className={`flex flex-row ${(message.role === 'system' || message.role === 'assistant') ? 'justify-start' : 'justify-end'}`}>
+        <div className={`  bg-gray-600 text-white m-1 p-1 rounded ${message.role === 'system' ? 'w-1/2' : 'w-3/4'}`}>
+
+          { content }
+          <br />
+          { parsedMessage !== "" && <button className="text-gray-400" onClick={handleRender}>Render snippet</button> }
+          {' '}
+        </div>
+      </div>
+    );
+  }
+  );
+
   return (
-    <pre className="bg-gray-800 text-white p-4 rounded h-full">
-      <code>
-{`
-  const greeting = 'Hello, world!';
-  console.log(greeting);
-`}
-      </code>
-    </pre>
+    <div className="bg-gray-800 text-white rounded h-full break-words overflow-auto max-h-400 max-w-full">
+      { messagesContent }
+    </div>
   );
 };
 
