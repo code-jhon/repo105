1ae75603665b4e165c7c7ccc2e6f49815e45c163
jhon avila <javilaortiz@gmail.com>
2023-07-09 22:52:08 -0500

    fix: unused variables

--- src/utils/hooks/useGPT.tsx
+++ src/utils/hooks/useGPT.tsx
@@ -1,15 +1,16 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Dispatch, SetStateAction, useEffect } from 'react'
 import API from '../../services/api';
 import { Message } from '../types';
 
 type useGPTType = (
-  prompt: string, messages: Message[], setMessages: Dispatch<SetStateAction<Message[]>>
+  messages: Message[], setMessages: Dispatch<SetStateAction<Message[]>>
   ) => { 
     messages: Message[]; 
     setMessages: Dispatch<SetStateAction<Message[]>> 
   }
 
-const useGPT: useGPTType = (prompt, messages, setMessages) => {
+const useGPT: useGPTType = (messages, setMessages) => {
   useEffect( () => {
     API.getChatCompletion(messages).then( (response) => {
       setMessages([...messages, { role: "system", content: response.toString() }]);
